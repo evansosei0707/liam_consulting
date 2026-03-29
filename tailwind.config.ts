f44b4e19c7812789042861446bd00d806c442a9f
Evans Osei <evansosei0707@gmail.com>
2026-03-29 17:06:56 +0000

    touched tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
     extend: {
       colors: {
         brand: {
-          950: "#17154d",
+          950: "#0A0F1C",
           900: "#0F172A",
           800: "#1A2341",
           700: "#243056",
